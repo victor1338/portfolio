534a612311870f46d16d31d286be86f9e742f132
victor1338 <victorlee1338@gmail.com>
2024-10-22 11:28:28 +0800

    auto-complete_card_seach

--- my-app/src/pages/hobby/Mtg.js
+++ my-app/src/pages/hobby/Mtg.js
@@ -1,18 +1,23 @@
 import React, { useEffect } from 'react';
 import {motion} from "framer-motion";
 import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 
 
 function Mtg() {
   const [Card,setCard]=useState(null);
   const [Cardimg,setCardimg]=useState(null);
+  const [CardName,setCardName]=useState("");
+  const [CardList,setCardList]=useState([]);
 
-  async function mtg_data(){
-    //fetch mtg data from scryfall
-    await fetch("https://api.scryfall.com/cards/random")
+
+  useEffect(()=>{
+    console.log("test_useEffect");
+    fetch("https://api.scryfall.com/cards/random")
     .then(response => {
       if (!response.ok) {
         throw new Error('Network response was not ok');
@@ -27,12 +32,7 @@ function Mtg() {
     })
     .catch(error => {
       console.error('Error:', error);
-    });
-  }
-
-  useEffect(()=>{
-    console.log("test_useEffect");
-      mtg_data();
+    }); 
     },[]
   )
 
@@ -43,12 +43,49 @@ function Mtg() {
   }
 
 
-  return(
-  <motion.div initial={ {opacity:0} } animate={{opacity:1}} transition={{duration:1}} >
-    <h1> {Card}</h1>
-    <img src={Cardimg}/>
+  const handleInput = (e,Newvalue)=> {
+    e.preventDefault();
+    setCardName(Newvalue);
+    console.log(CardName);
+    fetch_mtg_data();
+  }
 
+  const fetch_mtg_data =()=>{
+    if(CardName!==null){
+      if (CardName.includes(" ")){
+        setCardName(CardName.replace(" ","+"));}}
+    fetch("https://api.scryfall.com/cards/autocomplete?q="+CardName)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      setCardList(data.data);
+      console.log(CardList);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    })
+  }
+
+  return(
+  <motion.div >
+    <h1 > {Card}</h1>
+    <img src={Cardimg}/>
+    <Autocomplete
+      disablePortal
+      options={CardList}
+      sx={{ width: 300 }}
+      onInputChange={handleInput}
+      renderInput={(params) => <TextField {...params} label="Card" />}
+    />
+    {CardName}
   </motion.div>
+
+ 
   )
 };
 
